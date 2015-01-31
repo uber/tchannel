e9c3952117d3133429f6d530d0e589d24fd7d92f
Joshua T Corbin <joshua@uber.com>
2015-01-31 12:02:32 -0800

    Hoist responseFrameBuilder decoration

--- index.js
+++ index.js
@@ -539,7 +539,8 @@ TChannelConnection.prototype.handleReqFrame = function (reqFrame) {
 	}
 
 	this.inPending++;
-	var op = this.inOps[id] = new TChannelServerOp(this, handler, reqFrame, sendResponse);
+	var opCallback = responseFrameBuilder(reqFrame, sendResponse);
+	var op = this.inOps[id] = new TChannelServerOp(this, handler, reqFrame, opCallback);
 
 	function sendResponse(err, handlerErr, resFrame) {
 		if (err) {
@@ -608,7 +609,6 @@ function TChannelServerOp(connection, handler, reqFrame, callback) {
 	this.connection = connection;
 	this.handler = handler;
 	this.reqFrame = reqFrame;
-	callback = responseFrameBuilder(reqFrame, callback);
 	handler(reqFrame.arg2, reqFrame.arg3, connection.remoteName, callback);
 }
 
