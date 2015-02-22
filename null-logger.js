78c12e48ca9125c1f2ed3956ec770bac60cb7215
Joshua T Corbin <joshua@uber.com>
2015-02-22 12:05:17 -0800

    Add missing use stricts

--- null-logger.js
+++ null-logger.js
@@ -18,6 +18,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+'use strict';
+
 function noop() {}
 
 module.exports = {
--- parser.js
+++ parser.js
@@ -18,6 +18,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+'use strict';
+
 var farmhash = require('farmhash');
 
 /* jshint camelcase:false */
