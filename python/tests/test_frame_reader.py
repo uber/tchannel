05cd8a36eed13dd080d32002cfe9f21f41b0e82d
Aiden Scandella <aiden@uber.com>
2015-02-24 17:50:04 -0800

    Address review feedback

--- python/tests/test_frame_reader.py
+++ python/tests/test_frame_reader.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import
 import pytest
 
 from tchannel import exceptions
+from tchannel.frame_reader import FrameReader
 from tchannel.io import BytesIO
-from tchannel.stream_reader import StreamReader
 
 
 @pytest.fixture
@@ -13,7 +13,7 @@ def ping_request():
 
 def test_read_empty_buffer():
     """Verify we handle an empty buffer."""
-    reader = StreamReader(BytesIO(), chunk_size=4)
+    reader = FrameReader(BytesIO(), chunk_size=4)
     messages = [message for message in reader.read()]
     assert not messages
 
@@ -21,7 +21,7 @@ def test_read_empty_buffer():
 def test_read_invalid_size():
     """Verify we raise when we try to read but get nothing."""
     dummy_frame = b'\x00\x20' + b'\x00' * 14
-    reader = StreamReader(BytesIO(dummy_frame), chunk_size=len(dummy_frame))
+    reader = FrameReader(BytesIO(dummy_frame), chunk_size=len(dummy_frame))
 
     with pytest.raises(exceptions.ProtocolException):
         next(reader.read())
@@ -29,7 +29,7 @@ def test_read_invalid_size():
 
 def test_read_multi_chunk(ping_request):
     """Verify we read more from the stream when necessary."""
-    reader = StreamReader(BytesIO(ping_request), chunk_size=4)
+    reader = FrameReader(BytesIO(ping_request), chunk_size=4)
     next(reader.read())
 
 
@@ -41,6 +41,6 @@ def test_read_multi_chunk(ping_request):
 def test_read_multi_messages_one_chunk(ping_request, n):
     """Verify we read as many messages are in the chunk."""
     chunk = ping_request * n
-    reader = StreamReader(BytesIO(chunk), chunk_size=len(chunk))
+    reader = FrameReader(BytesIO(chunk), chunk_size=len(chunk))
     messages = [message for message in reader.read()]
     assert len(messages) == n
