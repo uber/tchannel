a5528235d3404f16ae0ffbb11a6e40bf14726319
Joshua T Corbin <joshua@uber.com>
2015-02-24 21:35:33 -0800

    Add readTest utility

---
+++ test/lib/readTest.js
@@ -0,0 +1,101 @@
+// Copyright (c) 2015 Uber Technologies, Inc.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+'use strict';
+
+var color = require('ansi-color').set;
+var hex = require('hexer');
+var util = require('util');
+
+module.exports = testRead;
+
+/* jshint maxparams:5 */
+
+function testRead(assert, reader, buffer, t, done) {
+    if (!done) done = assert.end;
+    var res = reader(buffer, 0);
+    var err = res[0];
+    var offset = res[1];
+    var val = res[2];
+    assert.error(err, 'should not fail to read');
+    assert.ok(offset === buffer.length, 'read entire buffer');
+    if (err || offset < buffer.length) {
+        console.log(hexHighlight(buffer, {
+            end: {
+                desc: 'read stopped at',
+                offset: offset,
+                color: 'red+bold'
+            }
+        }));
+        if (err) {
+            if (err.type) {
+                console.log(util.format('- with %s: %s',
+                    err.name, err.message));
+            } else {
+                console.log('- with error: ' + util.inspect(err));
+            }
+        }
+    }
+    if (val) {
+        t(val, assert);
+        done();
+    } else {
+        done(new Error('Expected to have read a value'));
+    }
+}
+
+function hexHighlight(buffer, highlights) {
+    var highlight = {};
+    var trail = '';
+
+    Object.keys(highlights).forEach(function eachHighlight(name) {
+        var h = highlights[name];
+        highlight[h.offset] = h.color;
+    });
+
+    var opts = {
+        decorateHexen: decorate,
+        decorateHuman: decorate
+    };
+    var out = hex(buffer, opts);
+
+    Object.keys(highlights).forEach(function eachHighlight(name) {
+        var h = highlights[name];
+        var off = h.offset.toString(16);
+        off = '0x' + pad('0', off, opts.offsetWidth);
+        trail += util.format('- %s: %s\n',
+            h.desc || name,
+            color(off, h.color));
+    });
+
+    out += '\n' + trail;
+    out = out.replace(/\n+$/, '');
+    return out;
+    function decorate(bufOffset, screenOffset, str) {
+        var c = highlight[bufOffset];
+        if (c) str = color(str, c);
+        return str;
+    }
+}
+
+function pad(c, s, width) {
+    while (s.length < width) s = c + s;
+    return s;
+}
