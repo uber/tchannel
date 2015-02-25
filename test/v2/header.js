7ec93c53af6b23f12547432c239a93bd1ab63df9
Joshua T Corbin <joshua@uber.com>
2015-02-25 14:14:13 -0800

    Add v2/header

---
+++ test/v2/header.js
@@ -0,0 +1,152 @@
+// Copyright (c) 2015 Uber Technologies, Inc.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+'use strict';
+
+var test = require('tape');
+var testRead = require('../lib/read_test.js');
+var Header = require('../../v2/header.js');
+
+var testEmpty = Buffer([
+    0x00 // nh:1
+]);
+
+var testSingle = Buffer([
+    0x01,                   // nh:1
+    0x03, 0x6b, 0x65, 0x79, // hk~1 "key"
+    0x03, 0x76, 0x61, 0x6c  // hv~1 "val"
+]);
+
+var testThree = Buffer([
+    0x03,                   // nh:1
+    0x03, 0x72, 0x65, 0x64, // hk~1 "red"
+    0x05, 0x67, 0x72, 0x65, // hv~1 "green"
+    0x65, 0x6e,             // ...
+    0x04, 0x62, 0x6c, 0x75, // hk~1 "blue"
+    0x65,                   // ...
+    0x06, 0x79, 0x65, 0x6c, // hv~1 "yellow"
+    0x6c, 0x6f, 0x77,       // ...
+    0x07, 0x6d, 0x61, 0x67, // hk~1 "magenta"
+    0x65, 0x6e, 0x74, 0x61, // ...
+    0x04, 0x63, 0x79, 0x61, // hv~1 "cyan"
+    0x6e                    // ...
+]);
+
+var dupKeyBuffer = Buffer([
+    0x02,                   // nh:1
+    0x03, 0x6b, 0x65, 0x79, // hk~1 "key"
+    0x03, 0x76, 0x61, 0x6c, // hv~1 "val"
+    0x03, 0x6b, 0x65, 0x79, // hk~1 "key"
+    0x03, 0x56, 0x41, 0x4c  // hv~1 "VAL"
+]);
+
+var testNullKey = Buffer([
+    0x01,                   // nh:1
+    0x00, 0x02, 0x65, 0x6b, // hk~1 hv~1 "" => "ek"
+]);
+
+var testNullVal = Buffer([
+    0x01,                   // nh:1
+    0x02, 0x65, 0x76, 0x00  // hk~1 hv~1 "ev" => ""
+]);
+
+test('read empty headers', function t(assert) {
+    testRead(assert, Header.read, testEmpty, function s(headers, done) {
+        var keys = Object.keys(headers);
+        assert.equal(keys.length, 0, 'expected empty headers');
+        done();
+    });
+});
+
+test('read singeton headers', function t(assert) {
+    testRead(assert, Header.read, testSingle, function s(headers, done) {
+        var keys = Object.keys(headers);
+        assert.deepEqual(keys, ['key'], 'expected one header');
+        assert.equal(headers.key, 'val', 'expected key => val');
+        done();
+    });
+});
+
+test('read three headers', function t(assert) {
+    testRead(assert, Header.read, testThree, function s(headers, done) {
+        var keys = Object.keys(headers);
+        assert.deepEqual(keys, ['red', 'blue', 'magenta'], 'expected one header');
+        assert.equal(headers.red, 'green', 'expected red => green');
+        assert.equal(headers.blue, 'yellow', 'expected blue => yellow');
+        assert.equal(headers.magenta, 'cyan', 'expected magenta => cyan');
+        done();
+    });
+});
+
+test('read duplicate key -> error', function t(assert) {
+    testRead.shouldError(assert, Header.read, dupKeyBuffer, function s(err, done) {
+        assert.equal(err.type, 'tchannel.duplicate-header-key', 'expected duplicate key error');
+        assert.equal(err.key, 'key');
+        assert.equal(err.value, 'VAL');
+        assert.equal(err.priorValue, 'val');
+        done();
+    });
+});
+
+test('read null key', function t(assert) {
+    testRead(assert, Header.read, testNullKey, function s(headers, done) {
+        var keys = Object.keys(headers);
+        assert.deepEqual(keys, [''], 'expected one header');
+        assert.equal(headers[''], 'ek', 'expected "" => "ek"');
+        done();
+    });
+});
+
+test('read null val', function t(assert) {
+    testRead(assert, Header.read, testNullVal, function s(headers, done) {
+        var keys = Object.keys(headers);
+        assert.deepEqual(keys, ['ev'], 'expected one header');
+        assert.equal(headers.ev, '', 'expected "ev" => ""');
+        done();
+    });
+});
+
+test('write empty headers', function t(assert) {
+    var headers = {};
+    assert.deepEqual(
+        Header.write(headers).create(), testEmpty,
+        'expected write output');
+    assert.end();
+});
+
+test('write singleton headers', function t(assert) {
+    var headers = {key: 'val'};
+    assert.deepEqual(
+        Header.write(headers).create(), testSingle,
+        'expected write output');
+    assert.end();
+});
+
+test('write three headers', function t(assert) {
+    var headers = {
+        red: 'green',
+        blue: 'yellow',
+        magenta: 'cyan'
+    };
+    assert.deepEqual(
+        Header.write(headers).create(), testThree,
+        'expected write output');
+    assert.end();
+});
---
+++ v2/header.js
@@ -0,0 +1,69 @@
+// Copyright (c) 2015 Uber Technologies, Inc.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+'use strict';
+
+var TypedError = require('error/typed');
+var read = require('../lib/read');
+var write = require('../lib/write');
+
+var DuplicateHeaderKeyError = TypedError({
+    type: 'tchannel.duplicate-header-key',
+    message: 'duplicate header key {key}',
+    key: null,
+    value: null,
+    priorValue: null
+});
+
+// nh:1 (hk~1 hv~1){nh}
+module.exports.read = read.chained(read.UInt8, function readHeaders(numHeaders, buffer, offset) {
+    var headers = {};
+    for (var i=0; i<numHeaders; i++) {
+        var res = read.pair1(buffer, offset);
+        if (res[0]) return res;
+        offset = res[1];
+        var pair = res[2];
+        var key = String(pair[0]);
+        var val = String(pair[1]);
+        if (headers[key] !== undefined) {
+            return [DuplicateHeaderKeyError({
+                key: key,
+                value: val,
+                priorValue: headers[key]
+            }), offset, null];
+        }
+        headers[key] = val;
+    }
+    return [null, offset, headers];
+});
+
+// nh:1 (hk~1 hv~1){nh}
+module.exports.write = function writeHeaders(headers) {
+    var keys = Object.keys(headers);
+    var parts = new Array(1 + 2 * keys.length);
+    parts[0] = write.UInt8(keys.length);
+    for (var i=0, j=0; i<keys.length; i++) {
+        var key = write.buf1(keys[i], 'header key');
+        var val = write.buf1(headers[keys[i]], 'header val');
+        parts[++j] = key;
+        parts[++j] = val;
+    }
+    return write.series(parts);
+};
