45b6222f0ba1eb6ee9309fb30e9b94988b5dabf1
Aiden Scandella <aiden@uber.com>
2015-02-27 12:11:08 -0800

    Add call request body round-tripping
    This implements happy-path round-tripping for CallRequest messages and
    fixes a bug or two along the way. It also adds a deserialization step to
    the message tests to ensure symmetry in implementations.

--- tchannel/messages/base.py
+++ tchannel/messages/base.py
@@ -1,5 +1,10 @@
 from __future__ import absolute_import
 
+from ..parser import read_key_value
+from ..parser import read_number
+from ..parser import write_number
+from ..parser import write_key_value
+
 
 class BaseMessage(object):
     """Represent common functionality across all TChannel messages."""
@@ -24,3 +29,31 @@ class BaseMessage(object):
         This generates the ``payload`` section of the message.
         """
         raise NotImplementedError()
+
+    def _read_headers(self, stream, nh_size, header_size):
+        """Read a variable number of headers.
+
+        Returns a tuple of (headers dict, bytes read)"""
+        num_headers = read_number(stream, nh_size)
+        bytes_read = nh_size
+
+        headers = {}
+        for _ in range(num_headers):
+            header_name, header_value, num_bytes = read_key_value(
+                stream,
+                header_size
+            )
+            headers[header_name] = header_value
+            bytes_read += num_bytes
+
+        return headers, bytes_read
+
+    def _write_headers(self, stream, headers, nh_size, header_size):
+        """Write number of headers followed by headers.
+
+        Format:
+            nh~nh_size (hk~header_size hv~header_size){nh}
+        """
+        stream.extend(write_number(len(headers), nh_size))
+        for key, value in headers.items():
+            stream.extend(write_key_value(key, value, key_size=header_size))
