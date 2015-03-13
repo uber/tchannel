92e4a0c987e36dbf92a93f6962272e04aa89b50e
Bryce Lampe <brycelampe@gmail.com>
2015-03-13 13:26:12 -0700

    make tornado server pong and respond with error on unknown endpoints

--- python/examples/tornado_server.py
+++ python/examples/tornado_server.py
@@ -7,6 +7,8 @@ import tornado.tcpserver
 
 from options import get_args
 from tchannel.messages import CallResponseMessage
+from tchannel.messages import ErrorMessage
+from tchannel.messages.types import Types
 from tchannel.tornado.connection import TornadoConnection
 
 
@@ -56,6 +58,15 @@ class MyServer(tornado.tcpserver.TCPServer):
 
             connection.frame_and_write(response)
 
+        elif context.message.message_type == Types.PING_REQ:
+            connection.pong()
+
+        else:
+            response = ErrorMessage()
+            response.code = 0x06
+            response.original_message_id = context.message_id
+            response.message = response.error_name()
+            connection.frame_and_write(response)
 
         connection.handle_calls(self.handle_call)
 
