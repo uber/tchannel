43988ed73fcbf54a53bb56e3e736ebef9a2caac0
Joshua T Corbin <joshua@uber.com>
2015-03-23 14:10:04 -0700

    v2/call: convert to ArgsRW

--- v2/handler.js
+++ v2/handler.js
@@ -285,9 +285,9 @@ TChannelV2Handler.prototype.buildIncomingRequest = function buildIncomingRequest
         service: reqFrame.body.service,
         headers: reqFrame.body.headers,
         checksumType: reqFrame.body.csum.type,
-        arg1: reqFrame.body.arg1,
-        arg2: reqFrame.body.arg2,
-        arg3: reqFrame.body.arg3
+        arg1: reqFrame.body.args[0],
+        arg2: reqFrame.body.args[1],
+        arg3: reqFrame.body.args[2]
     });
     return req;
 };
@@ -295,9 +295,9 @@ TChannelV2Handler.prototype.buildIncomingRequest = function buildIncomingRequest
 TChannelV2Handler.prototype.buildIncomingResponse = function buildIncomingResponse(resFrame) {
     var res = TChannelIncomingResponse(resFrame.id, {
         code: resFrame.body.code,
-        arg1: resFrame.body.arg1,
-        arg2: resFrame.body.arg2,
-        arg3: resFrame.body.arg3
+        arg1: resFrame.body.args[0],
+        arg2: resFrame.body.args[1],
+        arg3: resFrame.body.args[2]
     });
     return res;
 };
