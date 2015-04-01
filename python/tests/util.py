7980fcab424a6e2a919c5747d9f4a6e7873405d3
Bryce Lampe <brycelampe@gmail.com>
2015-04-01 12:34:02 -0700

    suppress binary output on test failures

--- python/tests/util.py
+++ python/tests/util.py
@@ -19,9 +19,9 @@
 # THE SOFTWARE.
 
 from __future__ import absolute_import
+
 from os import urandom
 
 
-def big_arg():
-    LARGE_AMOUNT = 64 * 1024 * 5
-    return urandom(LARGE_AMOUNT)
+def big_arg(kilobytes=64 * 5):
+    return urandom(1024 * kilobytes)
