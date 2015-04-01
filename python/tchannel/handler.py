8e377bfceb4e8b8c99932d4b30a03a3be4bc8383
Bryce Lampe <brycelampe@gmail.com>
2015-03-31 22:28:25 -0700

    support async handlers

--- python/tchannel/handler.py
+++ python/tchannel/handler.py
@@ -1,15 +1,15 @@
 # Copyright (c) 2015 Uber Technologies, Inc.
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -17,8 +17,12 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+from __future__ import absolute_import
 
 import collections
+
+import tornado
+
 from .messages import CallResponseMessage
 from .messages import PingRequestMessage
 from .messages import PingResponseMessage
@@ -62,11 +66,20 @@ class TChannelRequestHandler(RequestHandler):
         endpoint = self._find_endpoint(request.method)
         if endpoint is not None:
             response = TChannelResponse(request, conn)
+            result = None
             try:
-                endpoint.handler(request, response, endpoint.opts)
+                result = endpoint.handler(request, response, endpoint.opts)
+                if isinstance(result, tornado.gen.Future):
+                    result.add_done_callback(lambda f: response.finish())
+                    tornado.ioloop.IOLoop.current().add_future(
+                        result,
+                        lambda f: f.exception()
+                    )
+                return result
             # TODO add tchannel error handling here
             finally:
-                response.finish()
+                if result is None:
+                    response.finish()
 
         elif context.message.message_type == PingRequestMessage.message_type:
             response = TChannelResponse(request, conn)
---
+++ python/tests/test_handler.py
@@ -0,0 +1,83 @@
+# Copyright (c) 2015 Uber Technologies, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+from doubles import allow
+from doubles import expect
+from doubles import InstanceDouble
+import pytest
+import tornado
+
+from tchannel.handler import TChannelRequestHandler
+
+
+@pytest.fixture
+def handler():
+    return TChannelRequestHandler()
+
+
+@pytest.fixture
+def message():
+    message = InstanceDouble('tchannel.messages.base.BaseMessage')
+    message.message_type = 0
+    message.service = ''
+    message.arg_1 = 'test'
+    return message
+
+
+@pytest.fixture
+def context(message):
+    context = InstanceDouble('tchannel.context.Context')
+    allow(context).message.and_return(message)
+    allow(context).message_id.with_args().and_return(1)
+
+    return context
+
+
+@pytest.fixture
+def conn():
+    conn = InstanceDouble('tchannel.tornado.connection.TornadoConnection')
+    allow(conn).send_error
+    allow(conn).finish
+
+    return conn
+
+
+def test_sync_handler(handler, context, conn):
+
+    @handler.route("test")
+    def sync(request, response, opts):
+        response.write("done")
+
+    expect(conn).finish
+
+    handler.handle_request(context, conn)
+
+
+@pytest.mark.gen_test
+def test_async_handler(handler, context, conn):
+
+    @handler.route("test")
+    @tornado.gen.coroutine
+    def async(request, response, opts):
+        yield tornado.gen.sleep(0)
+        response.write("done")
+
+    expect(conn).finish
+
+    yield handler.handle_request(context, conn)
