2991b1addc06abd70d796a1767271e8ffb9be647
Abhinav Gupta <abg@uber.com>
2015-04-09 11:39:14 -0700

    Drop synchronous API for now.

--- python/tests/test_checksum.py
+++ python/tests/test_checksum.py
@@ -21,8 +21,6 @@
 from __future__ import absolute_import
 import pytest
 
-from tchannel import frame
-from tchannel.frame_reader import FrameReader
 from tchannel import messages
 from tchannel.messages import CallRequestMessage, ChecksumType
 from tchannel.io import BytesIO
@@ -37,20 +35,9 @@ def test_checksum(checksum_type, seed):
     message = CallRequestMessage()
     message.checksum = (checksum_type, seed)
     generate_checksum(message)
-    message_id = 32
     payload = messages.RW[message.message_type].write(
         message, BytesIO()
     ).getvalue()
 
-    f = frame.Frame(
-        header=frame.FrameHeader(
-            message_type=message.message_type,
-            message_id=message_id,
-        ),
-        payload=payload
-    )
-
-    inframe = frame.frame_rw.write(f, BytesIO()).getvalue()
-    reader = FrameReader(BytesIO(inframe))
-    msg = reader.read().next().message
+    msg = messages.RW[message.message_type].read(BytesIO(payload))
     assert verify_checksum(msg)
