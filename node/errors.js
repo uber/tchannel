6a1038640859bb8527939e45bf2225f126e1c291
Joshua T Corbin <joshua@uber.com>
2015-04-10 15:42:15 -0700

    Fix typo in error type name

--- node/errors.js
+++ node/errors.js
@@ -147,7 +147,7 @@ module.exports.TopLevelRegisterError = TypedError({
 });
 
 module.exports.TopLevelRequestError = TypedError({
-    type: 'tchyannel.top-level-request',
+    type: 'tchannel.top-level-request',
     message: 'Cannot make request() on top level tchannel without host.\n' +
         'Must set a direct host or use a sub channel.'
 });
