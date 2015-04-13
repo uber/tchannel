034af5012ee277ae434499c0e9a80e950fb81e9c
Raynos <raynos2@gmail.com>
2015-04-13 16:02:07 -0700

    remove legacy register() support

--- node/examples/legacy_example.js
+++ node/examples/legacy_example.js
@@ -26,36 +26,39 @@ var CountedReadySignal = require('ready-signal/counted');
 var TChannel = require('../channel.js');
 
 var ready = CountedReadySignal(2);
-var server = new TChannel();
+var server = new TChannel({
+    serviceName: 'server'
+});
 server.listen(4040, '127.0.0.1', ready.signal);
 var client = new TChannel();
 client.listen(4041, '127.0.0.1', ready.signal);
 
 // normal response
-server.register('func 1', function func1(arg1, arg2, peerInfo, cb) {
+server.register('func 1', function func1(req, res, arg2, arg3) {
     console.log('func 1 responding immediately 1:' +
-        arg1.toString() + ' 2:' + arg2.toString());
-    cb(null, 'result', 'indeed it did');
+        arg2.toString() + ' 2:' + arg3.toString());
+    res.sendOk('result', 'indeed it did');
 });
 
 // err response
-server.register('func 2', function func2(arg1, arg2, peerInfo, cb) {
-    cb(new Error('it failed'));
+server.register('func 2', function func2(req, res, arg2, arg3) {
+    res.sendNotOk(null, 'it failed');
 });
 
 ready(function onReady() {
     client.send({
         host: '127.0.0.1:4040'
-    }, 'func 1', 'arg 1', 'arg 2', function onResp1(err, res1, res2) {
+    }, 'func 1', 'arg 1', 'arg 2', function onResp1(err, res, res1, res2) {
         if (err) {
             console.log('unexpected err: ' + err.message);
+        } else {
+            console.log('normal res: ' + res1.toString() + ' ' + res2.toString());
         }
-        console.log('normal res: ' + res1.toString() + ' ' + res2.toString());
     });
 
     client.send({
         host: '127.0.0.1:4040'
-    }, 'func 2', 'arg 1', 'arg 2', function onResp2(err, res1, res2) {
-        console.log('err res: ' + err.message);
+    }, 'func 2', 'arg 1', 'arg 2', function onResp2(err, res, res1, res2) {
+        console.log('ok: ' + res.ok + ' err res: ' + res2.toString());
     });
 });
