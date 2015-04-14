69483aa311c96f8b6041c53890cd4b554cb357bf
Joshua T Corbin <joshua@uber.com>
2015-04-14 08:38:18 -0700

    benchmark: generalize client arg passing

--- benchmarks/compare_to.sh
+++ benchmarks/compare_to.sh
@@ -2,12 +2,11 @@
 set -e
 set -x
 
-multiplicity=5
-
+args=
 while getopts m: OPT; do
     case $OPT in
         m)
-        multiplicity=$OPTARG
+        args+=" -$OPT $OPTARG"
         ;;
     esac
 done
@@ -21,7 +20,7 @@ cur_branch=$(basename $(git symbolic-ref HEAD))
 
 for branch in $base_branch $cur_branch; do
     git checkout $branch
-    node $bench_dir/index.js -m $multiplicity -o benchmarks/$branch.json
+    node $bench_dir/index.js -o benchmarks/$branch.json -- $args
 done
 
 node $bench_dir/compare.js $bench_dir/$base_branch.json $bench_dir/$cur_branch.json
