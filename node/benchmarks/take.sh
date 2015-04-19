f1765467995455ef9d57312ba005479b8fb462cb
Joshua T Corbin <joshua@uber.com>
2015-04-19 14:33:06 -0700

    Explicate take benchmark script

---
+++ node/benchmarks/take.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+set -x
+
+rev=$(git rev-parse --short HEAD)
+sym=$(git symbolic-ref HEAD)
+sym=${sym##*/}
+
+node benchmarks/index.js -o benchmarks/$rev.json -- -m 5 -p 1000,10000,20000 -s 4,$(( 2 ** 12 )),$(( 2 ** 14 ))
+ln -sf $rev.json benchmarks/$sym.json
