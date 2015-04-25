a884e3ce8a5c26da86836b94bdcddd370363420f
Kai Wei <kwei@uber.com>
2015-04-24 17:02:15 -0700

    a streaming implementation of the thrift protocols
    This removes the double buffering. However, for some reason this impl seems to be much slower (2 orders of magnitude).

--- golang/examples/thrift/server_client_test.go
+++ golang/examples/thrift/server_client_test.go
@@ -2,6 +2,7 @@ package thrift
 
 import "testing"
 import "fmt"
+import appachethrift "github.com/apache/thrift/lib/go/thrift"
 import "github.com/uber/tchannel/golang/thrift"
 import gen "github.com/uber/tchannel/golang/examples/thrift/gen-go/test"
 import "errors"
@@ -10,15 +11,179 @@ import "golang.org/x/net/context"
 import "time"
 import tchannel "github.com/uber/tchannel/golang"
 
-func newFirstClient(server *thrift.Server, t *testing.T) *gen.FirstClient {
+func TestEcho(t *testing.T) {
+	withTestServersAndClients(t, func(server *thrift.Server, client *gen.FirstClient) {
+		testEcho(server, client, t)
+	})
+}
+
+func testEcho(server *thrift.Server, client *gen.FirstClient, t *testing.T) {
+	for i := 0; i < 3; i++ {
+		msg := fmt.Sprintf("thrift over tchannel #%d", i)
+		if res, err := client.Echo(msg); err != nil {
+			t.Fatal("Echo failed", err)
+		} else if res != msg {
+			t.Errorf("Echo returned unexpected result: %v", res)
+		}
+	}
+}
+
+func TestHealthcheck(t *testing.T) {
+	withTestServersAndClients(t, func(server *thrift.Server, client *gen.FirstClient) {
+		testHealthcheck(server, client, t)
+	})
+}
+
+func testHealthcheck(server *thrift.Server, client *gen.FirstClient, t *testing.T) {
+	for i := 0; i < 3; i++ {
+		if res, err := client.Healthcheck(); err != nil {
+			t.Fatal("Healthcheck failed", err)
+		} else if !res.Healthy || res.Msg != "OK" {
+			t.Errorf("Healthcheck returned unexpected result: %v", res)
+		}
+	}
+}
+
+func TestAppError(t *testing.T) {
+	withTestServersAndClients(t, func(server *thrift.Server, client *gen.FirstClient) {
+		testAppError(server, client, t)
+	})
+}
+
+func testAppError(server *thrift.Server, client *gen.FirstClient, t *testing.T) {
+	for i := 0; i < 3; i++ {
+		if err := client.AppError(); err == nil {
+			t.Errorf("AppError should return error but did not")
+		}
+	}
+}
+
+func TestOneWay(t *testing.T) {
+	withTestServersAndClients(t, func(server *thrift.Server, client *gen.FirstClient) {
+		testOneWay(server, client, t)
+	})
+}
+
+func testOneWay(server *thrift.Server, client *gen.FirstClient, t *testing.T) {
+	for i := 0; i < 3; i++ {
+		if err := client.OneWay(); err != nil {
+			t.Fatal("OneWay failed", err)
+		}
+	}
+}
+
+func TestAll(t *testing.T) {
+	withTestServersAndClients(t, func(server *thrift.Server, client *gen.FirstClient) {
+		testAll(server, client, t)
+	})
+}
+
+func testAll(server *thrift.Server, firstClient *gen.FirstClient, t *testing.T) {
+	secondClient := newSecondClient(server, t)
+	for i := 0; i < 3; i++ {
+		msg := fmt.Sprintf("thrift over tchannel #%d", i)
+		if res, err := firstClient.Echo(msg); err != nil {
+			t.Fatal("Echo failed", err)
+		} else if res != msg {
+			t.Errorf("Echo returned unexpected result: %v", res)
+		}
+
+		if res, err := firstClient.Healthcheck(); err != nil {
+			t.Fatal("Healthcheck failed", err)
+		} else if !res.Healthy || res.Msg != "OK" {
+			t.Errorf("Healthcheck returned unexpected result: %v", res)
+		}
+
+		if err := firstClient.AppError(); err == nil {
+			t.Errorf("AppError should return error but did not")
+		}
+
+		if err := firstClient.OneWay(); err != nil {
+			t.Fatal("OneWay failed", err)
+		}
+
+		if err := secondClient.Test(); err != nil {
+			t.Fatal("Test failed", err)
+		}
+	}
+}
+
+func TestBadClient(t *testing.T) {
+	withTestServer(t, true, func(server *thrift.Server) {
+		testBadClient(server, t)
+	})
+	withTestServer(t, false, func(server *thrift.Server) {
+		testBadClient(server, t)
+	})
+}
+
+func testBadClient(server *thrift.Server, t *testing.T) {
+	client := newBadClient(server, t)
+	for i := 0; i < 3; i++ {
+		msg := fmt.Sprintf("thrift over tchannel #%d", i)
+		if _, err := client.Echo(msg); err == nil {
+			t.Error("Echo should have failed but did not")
+		}
+
+		if _, err := client.Healthcheck(); err == nil {
+			t.Error("Healthcheck should have failed but did not")
+		}
+
+		if err := client.AppError(); err == nil {
+			t.Errorf("AppError should have failed but did not")
+		}
+	}
+}
+
+func withTestServer(t *testing.T, buffered bool, f func(s *thrift.Server)) {
+	server, err := thrift.NewServer(":0", "MyThriftService", thrift.ServerOptions{Buffered: buffered})
+	if err != nil {
+		t.Fatal("Failed to create server", err)
+	}
+
+	firstHandler := FirstHandler{}
+	server.Register("FirstProcessor", reflect.TypeOf(&firstHandler), gen.NewFirstProcessor(&firstHandler))
+	secondHandler := SecondHandler{}
+	server.Register("SecondProcessor", reflect.TypeOf(&secondHandler), gen.NewSecondProcessor(&secondHandler))
+	go server.ListenAndServe()
+
+	defer server.Stop()
+	f(server)
+}
+
+func withTestServerAndClient(t *testing.T, bufferedServer, bufferedClient bool,
+	f func(s *thrift.Server, c *gen.FirstClient)) {
+
+	withTestServer(t, bufferedServer, func(server *thrift.Server) {
+		client := newFirstClient(server, bufferedClient, t)
+		f(server, client)
+	})
+}
+
+func withTestServersAndClients(t *testing.T, f func(s *thrift.Server, c *gen.FirstClient)) {
+	for _, bufferedServer := range []bool{true, false} {
+		for _, bufferedClient := range []bool{true, false} {
+			withTestServerAndClient(t, bufferedServer, bufferedClient, f)
+		}
+	}
+}
+
+func newFirstClient(server *thrift.Server, buffered bool, t *testing.T) *gen.FirstClient {
 	channel, err := tchannel.NewChannel(":0", nil)
 	if err != nil {
 		t.Fatal("Failed to create tchannel")
 	}
 	timeout := time.Second * time.Duration(1) // per-call timeout
-	protocol, err := thrift.NewTChannelOutboundProtocol(context.Background(), channel,
-		server.HostPort(), "MyThriftService", "FirstProcessor",
-		thrift.OutboundOptions{Timeout: &timeout})
+	var protocol appachethrift.TProtocol
+	if buffered {
+		protocol, err = thrift.NewTChannelOutboundProtocol(context.Background(), channel,
+			server.HostPort(), "MyThriftService", "FirstProcessor",
+			thrift.OutboundOptions{Timeout: &timeout})
+	} else {
+		protocol, err = thrift.NewTChannelOutboundProtocolStreamed(context.Background(), channel,
+			server.HostPort(), "MyThriftService", "FirstProcessor",
+			thrift.OutboundOptions{Timeout: &timeout})
+	}
 	if err != nil {
 		t.Fatal("Failed to create client")
 	}
@@ -32,7 +197,7 @@ func newSecondClient(server *thrift.Server, t *testing.T) *gen.SecondClient {
 	}
 	timeout := time.Second * time.Duration(2) // overall timeout
 	ctx, _ := context.WithTimeout(context.Background(), timeout)
-	protocol, err := thrift.NewTChannelOutboundProtocol(ctx, channel,
+	protocol, err := thrift.NewTChannelOutboundProtocolStreamed(ctx, channel,
 		server.HostPort(), "MyThriftService", "SecondProcessor",
 		thrift.OutboundOptions{})
 	if err != nil {
@@ -47,131 +212,13 @@ func newBadClient(server *thrift.Server, t *testing.T) *gen.FirstClient {
 		t.Fatal("Failed to create tchannel")
 	}
 	timeout := time.Second * time.Duration(1)
-	protocol, err := thrift.NewTChannelOutboundProtocol(context.Background(), channel,
+	protocol, err := thrift.NewTChannelOutboundProtocolStreamed(context.Background(), channel,
 		server.HostPort(), "MyThriftService", "ThirdProcessor",
 		thrift.OutboundOptions{Timeout: &timeout})
 	if err != nil {
 		t.Fatal("Failed to create client")
 	}
 	return gen.NewFirstClientProtocol(nil, protocol, protocol)
-}
-
-func TestEcho(t *testing.T) {
-	withTestServer(t, func(server *thrift.Server) {
-		client := newFirstClient(server, t)
-		for i := 0; i < 3; i++ {
-			msg := fmt.Sprintf("thrift over tchannel #%d", i)
-			if res, err := client.Echo(msg); err != nil {
-				t.Fatal("Echo failed", err)
-			} else if res != msg {
-				t.Errorf("Echo returned unexpected result: %v", res)
-			}
-		}
-	})
-}
-
-func TestHealthcheck(t *testing.T) {
-	withTestServer(t, func(server *thrift.Server) {
-		client := newFirstClient(server, t)
-		for i := 0; i < 3; i++ {
-			if res, err := client.Healthcheck(); err != nil {
-				t.Fatal("Healthcheck failed", err)
-			} else if !res.Healthy || res.Msg != "OK" {
-				t.Errorf("Healthcheck returned unexpected result: %v", res)
-			}
-		}
-	})
-}
-
-func TestAppError(t *testing.T) {
-	withTestServer(t, func(server *thrift.Server) {
-		client := newFirstClient(server, t)
-		for i := 0; i < 3; i++ {
-			if err := client.AppError(); err == nil {
-				t.Errorf("AppError should return error but did not")
-			}
-		}
-	})
-}
-
-func TestOneWay(t *testing.T) {
-	withTestServer(t, func(server *thrift.Server) {
-		client := newFirstClient(server, t)
-		for i := 0; i < 3; i++ {
-			if err := client.OneWay(); err != nil {
-				t.Fatal("OneWay failed", err)
-			}
-		}
-	})
-}
-
-func TestAll(t *testing.T) {
-	withTestServer(t, func(server *thrift.Server) {
-		firstClient := newFirstClient(server, t)
-		secondClient := newSecondClient(server, t)
-		for i := 0; i < 3; i++ {
-			msg := fmt.Sprintf("thrift over tchannel #%d", i)
-			if res, err := firstClient.Echo(msg); err != nil {
-				t.Fatal("Echo failed", err)
-			} else if res != msg {
-				t.Errorf("Echo returned unexpected result: %v", res)
-			}
-
-			if res, err := firstClient.Healthcheck(); err != nil {
-				t.Fatal("Healthcheck failed", err)
-			} else if !res.Healthy || res.Msg != "OK" {
-				t.Errorf("Healthcheck returned unexpected result: %v", res)
-			}
-
-			if err := firstClient.AppError(); err == nil {
-				t.Errorf("AppError should return error but did not")
-			}
-
-			if err := firstClient.OneWay(); err != nil {
-				t.Fatal("OneWay failed", err)
-			}
-
-			if err := secondClient.Test(); err != nil {
-				t.Fatal("Test failed", err)
-			}
-		}
-	})
-}
-
-func TestBadClient(t *testing.T) {
-	withTestServer(t, func(server *thrift.Server) {
-		client := newBadClient(server, t)
-		for i := 0; i < 3; i++ {
-			msg := fmt.Sprintf("thrift over tchannel #%d", i)
-			if _, err := client.Echo(msg); err == nil {
-				t.Error("Echo should have failed but did not")
-			}
-
-			if _, err := client.Healthcheck(); err == nil {
-				t.Error("Healthcheck should have failed but did not")
-			}
-
-			if err := client.AppError(); err == nil {
-				t.Errorf("AppError should have failed but did not")
-			}
-		}
-	})
-}
-
-func withTestServer(t *testing.T, f func(s *thrift.Server)) {
-	server, err := thrift.NewServer(":0", "MyThriftService")
-	if err != nil {
-		t.Fatal("Failed to create server", err)
-	}
-
-	firstHandler := FirstHandler{}
-	server.Register("FirstProcessor", reflect.TypeOf(&firstHandler), gen.NewFirstProcessor(&firstHandler))
-	secondHandler := SecondHandler{}
-	server.Register("SecondProcessor", reflect.TypeOf(&secondHandler), gen.NewSecondProcessor(&secondHandler))
-	go server.ListenAndServe()
-
-	defer server.Stop()
-	f(server)
 }
 
 type FirstHandler struct {
--- golang/thrift/server.go
+++ golang/thrift/server.go
@@ -9,6 +9,11 @@ import "github.com/apache/thrift/lib/go/thrift"
 type Server struct {
 	serviceName string
 	tchannel    *tchannel.Channel
+	options     ServerOptions
+}
+
+type ServerOptions struct {
+	Buffered bool
 }
 
 // Register registers the given processor's methods as handlers with this server.
@@ -16,7 +21,7 @@ type Server struct {
 // under the operations 'myProcessor::foo' and 'myProcessor::bar'.
 func (s *Server) Register(processorName string, processorInterface reflect.Type, processor thrift.TProcessor) {
 	// all methods of the processor is handled by the same handler
-	handler := &ThriftHandler{processor}
+	handler := &ThriftHandler{processor, s.options.Buffered}
 	for i := 0; i < processorInterface.NumMethod(); i++ {
 		methodName := processorInterface.Method(i).Name
 		s.tchannel.Register(handler, s.serviceName, processorName+"::"+methodName)
@@ -36,27 +41,33 @@ func (s *Server) HostPort() string {
 }
 
 // NewServer creates a service that speaks thrift over tchannel.
-func NewServer(bindAddr, serviceName string) (*Server, error) {
+func NewServer(bindAddr, serviceName string, options ServerOptions) (*Server, error) {
 	ch, err := tchannel.NewChannel(bindAddr, &tchannel.ChannelOptions{})
 	if err != nil {
 		return nil, err
 	}
 
-	return &Server{serviceName, ch}, nil
+	return &Server{serviceName, ch, options}, nil
 }
 
 // NewServerWithChannel creates a service that speaks thrift over tchannel.
-func NewServerWithChannel(serviceName string, ch *tchannel.Channel) *Server {
-	return &Server{serviceName, ch}
+func NewServerWithChannel(serviceName string, ch *tchannel.Channel, options ServerOptions) *Server {
+	return &Server{serviceName, ch, options}
 }
 
 // ThriftHandler wraps the tchannel.Handler interface
 // around the thrift processor
 type ThriftHandler struct {
 	processor thrift.TProcessor
+	buffered  bool
 }
 
 func (s *ThriftHandler) Handle(ctx context.Context, call *tchannel.InboundCall) {
-	protocol := NewTChannelInboundProtocol(call)
+	var protocol thrift.TProtocol
+	if s.buffered {
+		protocol = NewTChannelInboundProtocol(call)
+	} else {
+		protocol = NewTChannelInboundProtocolStreamed(call)
+	}
 	s.processor.Process(protocol, protocol)
 }
