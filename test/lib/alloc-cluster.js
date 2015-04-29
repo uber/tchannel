ae64d1f59807bf9b09a4c898b41b4e61aaaf2c48
Joshua T Corbin <joshua@uber.com>
2015-04-29 12:06:51 -0700

    test/lib: factor ResourcePool out of allocCluster

--- test/lib/alloc-cluster.js
+++ test/lib/alloc-cluster.js
@@ -20,7 +20,6 @@
 
 'use strict';
 
-var async = require('async');
 var extend = require('xtend');
 var CountedReadySignal = require('ready-signal/counted');
 var test = require('tape');
@@ -167,33 +166,4 @@ allocCluster.test.only = function testClusterOnly(desc, opts, t) {
     });
 };
 
-function ClusterPool(setup) {
-    var self = this;
-    self.free = [];
-    self.setup = setup;
-}
-
-ClusterPool.prototype.get = function getCluster(callback) {
-    var self = this;
-    if (self.free.length) {
-        callback(null, self.free.shift());
-    } else {
-        self.setup(callback);
-    }
-};
-
-ClusterPool.prototype.release = function release(cluster) {
-    var self = this;
-    self.free.push(cluster);
-};
-
-ClusterPool.prototype.destroy = function destroy(callback) {
-    var self = this;
-    var free = self.free;
-    self.free = [];
-    async.each(free, function destroyEach(cluster, next) {
-        cluster.destroy(next);
-    }, callback);
-};
-
-allocCluster.Pool = ClusterPool;
+allocCluster.Pool = require('./resource_pool');
