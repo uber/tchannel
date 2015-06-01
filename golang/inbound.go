e6a92d97fb3f26eaeae88d078ddf84729acf07c2
Matt Mihic <mmihic@uber.com>
2015-05-31 22:50:20 -0400

    DRY req/res parsing/writing

--- golang/inbound.go
+++ golang/inbound.go
@@ -31,14 +31,13 @@ var (
 	// ErrHandlerNotFound is returned when no registered handler can be found for a given service and operation
 	ErrHandlerNotFound = NewSystemError(ErrCodeBadRequest, "no handler for service and operation")
 
-	errCallStateMismatch           = errors.New("attempting to read / write outside of expected state")
 	errInboundRequestAlreadyActive = errors.New("inbound request is already active; possible duplicate client id")
 )
 
 // Handles an incoming call request, dispatching the call to the worker pool
 func (c *Connection) handleCallReq(frame *Frame) {
-	callReq := callReq{id: frame.Header.ID}
-	initialFragment, err := newReadableFragment(frame, &callReq)
+	callReq := new(callReq)
+	initialFragment, err := parseInboundFragment(frame, callReq)
 	if err != nil {
 		// TODO(mmihic): Probably want to treat this as a protocol error
 		c.log.Errorf("could not decode %s: %v", frame.Header, err)
@@ -49,33 +48,40 @@ func (c *Connection) handleCallReq(frame *Frame) {
 	ctx, cancel := context.WithTimeout(context.Background(), callReq.TimeToLive)
 	ctx = context.WithValue(ctx, tracingKey, &callReq.Tracing)
 
-	mex, err := c.inbound.newExchange(ctx, callReq.messageType(), callReq.ID(), 512)
+	mex, err := c.inbound.newExchange(ctx, callReq.messageType(), frame.Header.ID, 512)
 	if err != nil {
 		c.log.Errorf("could not register exchange for %s", frame.Header)
 		return
 	}
 
-	res := &InboundCallResponse{
-		id:     frame.Header.ID,
-		state:  inboundCallResponseReadyToWriteArg1,
-		conn:   c,
-		cancel: cancel,
-		mex:    mex,
-		span:   callReq.Tracing,
-	}
-	res.body = newFragmentingWriter(res, initialFragment.checksumType.New())
+	response := new(InboundCallResponse)
+	response.mex = mex
+	response.conn = c
+	response.contents = newFragmentingWriter(response, initialFragment.checksumType.New())
+	response.cancel = cancel
+	response.span = callReq.Tracing
+	response.messageForFragment = func(initial bool) message {
+		if initial {
+			callRes := new(callRes)
+			callRes.Headers = callHeaders{}
+			callRes.ResponseCode = responseOK
+			if response.applicationError {
+				callRes.ResponseCode = responseApplicationError
+			}
+			return callRes
+		}
 
-	call := &InboundCall{
-		id:              frame.Header.ID,
-		conn:            c,
-		res:             res,
-		mex:             mex,
-		initialFragment: initialFragment,
-		serviceName:     string(callReq.Service),
-		state:           inboundCallReadyToReadArg1,
-		span:            callReq.Tracing,
+		return new(callResContinue)
 	}
-	call.body = newFragmentingReader(call)
+
+	call := new(InboundCall)
+	call.mex = mex
+	call.initialFragment = initialFragment
+	call.serviceName = string(callReq.Service)
+	call.span = callReq.Tracing
+	call.response = response
+	call.messageForFragment = func(initial bool) message { return new(callReqContinue) }
+	call.contents = newFragmentingReader(call)
 
 	go c.dispatchInbound(call)
 }
@@ -98,7 +104,7 @@ func (c *Connection) dispatchInbound(call *InboundCall) {
 
 	if err := call.readOperation(); err != nil {
 		c.log.Errorf("Could not read operation from %s: %v", c.remotePeerInfo, err)
-		c.inboundCallComplete(call.id)
+		c.inboundCallComplete(call.mex.msgID)
 		return
 	}
 
@@ -119,27 +125,13 @@ func (c *Connection) dispatchInbound(call *InboundCall) {
 
 // An InboundCall is an incoming call from a peer
 type InboundCall struct {
-	id              uint32
-	conn            *Connection
-	res             *InboundCallResponse
-	mex             *messageExchange
-	serviceName     string
-	operation       []byte
-	state           inboundCallState
-	initialFragment *readableFragment
-	body            *fragmentingReader
-	span            Span
+	reqResReader
+
+	response    *InboundCallResponse
+	serviceName string
+	operation   []byte
+	span        Span
 }
-
-type inboundCallState int
-
-const (
-	inboundCallReadyToReadArg1 inboundCallState = iota
-	inboundCallReadyToReadArg2
-	inboundCallReadyToReadArg3
-	inboundCallAllRead
-	inboundCallError
-)
 
 // ServiceName returns the name of the service being called
 func (call *InboundCall) ServiceName() string {
@@ -154,121 +146,66 @@ func (call *InboundCall) Operation() []byte {
 // Reads the entire operation name (arg1) from the request stream.
 func (call *InboundCall) readOperation() error {
 	var arg1 BytesInput
-	if err := call.readArg(&arg1, false, inboundCallReadyToReadArg1, inboundCallReadyToReadArg2); err != nil {
-		return err
+	if err := call.readArg1(&arg1); err != nil {
+		return call.failed(err)
 	}
 
 	call.operation = arg1
 	return nil
 }
 
-// ReadArg2 reads the second argument from the inbound call, blocking until the entire
-// argument has been read or an error/timeout occurs.
+// ReadArg2 reads the second argument from the request, blocking until the
+// argument is ready or an error/timeout has occurred
 func (call *InboundCall) ReadArg2(arg Input) error {
-	return call.readArg(arg, false, inboundCallReadyToReadArg2, inboundCallReadyToReadArg3)
+	return call.readArg2(arg)
 }
 
-// ReadArg3 reads the third argument from the inbound call, blocking until th entire
-// argument has been read or an error/timeout occurs.
+// ReadArg3 reads the third argument from the request, blocking until the
+// argument is ready or an error/timeout has occurred.
 func (call *InboundCall) ReadArg3(arg Input) error {
-	return call.readArg(arg, true, inboundCallReadyToReadArg3, inboundCallAllRead)
-}
-
-// failed marks the call as failed
-func (call *InboundCall) failed(err error) error {
-	call.state = inboundCallError
-	call.conn.inboundCallComplete(call.id)
-	return err
-}
-
-// readArg reads an argument from the call, assuming the call is in a given
-// initial state.  Leaves the call in the provided output state
-func (call *InboundCall) readArg(arg Input, last bool, inState inboundCallState, outState inboundCallState) error {
-	if call.state != inState {
-		return call.failed(errCallStateMismatch)
-	}
-
-	if err := call.body.ReadArgument(arg, last); err != nil {
-		return call.failed(err)
-	}
-
-	call.state = outState
-	return nil
-}
-
-// recvNextFragment returns the next incoming fragment for the call.  If this
-// is the first fragment, we use the callReq that initiated the call.
-// Otherwise we wait for more fragments to arrive from the peer through the
-// mex
-func (call *InboundCall) recvNextFragment(initial bool) (*readableFragment, error) {
-	if initial {
-		fragment := call.initialFragment
-		call.initialFragment = nil
-		return fragment, nil
-	}
-
-	msg := message(new(callReqContinue))
-	frame, err := call.mex.recvPeerFrameOfType(msg.messageType())
-	if err != nil {
-		return nil, err
-	}
-
-	return newReadableFragment(frame, msg)
+	return call.readArg3(arg)
 }
 
 // Response provides access to the InboundCallResponse object which can be used
 // to write back to the calling peer
 func (call *InboundCall) Response() *InboundCallResponse {
-	return call.res
+	return call.response
 }
 
 // An InboundCallResponse is used to send the response back to the calling peer
 type InboundCallResponse struct {
-	id               uint32
-	mex              *messageExchange
+	reqResWriter
+
 	cancel           context.CancelFunc
-	conn             *Connection
-	body             *fragmentingWriter
-	state            inboundCallResponseState
 	applicationError bool
 	span             Span
 }
 
-type inboundCallResponseState int
-
-const (
-	inboundCallResponseReadyToWriteArg1 inboundCallResponseState = iota
-	inboundCallResponseReadyToWriteArg2
-	inboundCallResponseReadyToWriteArg3
-	inboundCallResponseComplete
-	inboundCallResponseError
-)
-
 // SendSystemError returns a system error response to the peer.  The call is considered
 // complete after this method is called, and no further data can be written.
-func (call *InboundCallResponse) SendSystemError(err error) error {
+func (response *InboundCallResponse) SendSystemError(err error) error {
 	// Fail all future attempts to read fragments
-	call.cancel()
-	call.state = inboundCallResponseComplete
+	response.cancel()
+	response.state = reqResWriterComplete
 
 	// Send the error frame
-	frame := call.conn.framePool.Get()
+	frame := response.conn.framePool.Get()
 	if err := frame.write(&errorMessage{
-		id:      call.id,
-		tracing: call.span,
+		id:      response.mex.msgID,
+		tracing: response.span,
 		errCode: GetSystemErrorCode(err),
 		message: err.Error()}); err != nil {
 		// Nothing we can do here
-		call.conn.log.Warnf("Could not create outbound frame to %s for %d: %v",
-			call.conn.remotePeerInfo, call.id, err)
+		response.conn.log.Warnf("Could not create outbound frame to %s for %d: %v",
+			response.conn.remotePeerInfo, response.mex.msgID, err)
 		return nil
 	}
 
 	select {
-	case call.conn.sendCh <- frame: // Good to go
+	case response.conn.sendCh <- frame: // Good to go
 	default: // Nothing we can do here anyway
-		call.conn.log.Warnf("Could not send error frame to %s for %d : %v",
-			call.conn.remotePeerInfo, call.id, err)
+		response.conn.log.Warnf("Could not send error frame to %s for %d : %v",
+			response.conn.remotePeerInfo, response.mex.msgID, err)
 	}
 
 	return nil
@@ -276,96 +213,28 @@ func (call *InboundCallResponse) SendSystemError(err error) error {
 
 // SetApplicationError marks the response as being an application error.  This method can
 // only be called before any arguments have been sent to the calling peer.
-func (call *InboundCallResponse) SetApplicationError() error {
-	if call.state != inboundCallResponseReadyToWriteArg2 {
-		return call.failed(errCallStateMismatch)
+func (response *InboundCallResponse) SetApplicationError() error {
+	if response.state > reqResWriterPreArg2 {
+		return response.failed(errReqResReaderStateMismatch)
 	}
-
-	call.applicationError = true
+	response.applicationError = true
 	return nil
-}
-
-// writeOperation writes the operation.
-func (call *InboundCallResponse) writeOperation(operation []byte) error {
-	return call.writeArg(BytesOutput(operation), false, inboundCallResponseReadyToWriteArg1,
-		inboundCallResponseReadyToWriteArg2)
 }
 
 // WriteArg2 writes the second argument in the response, blocking until the argument is
 // fully written or an error/timeout has occurred.
-func (call *InboundCallResponse) WriteArg2(arg Output) error {
-	if err := call.writeOperation(nil); err != nil {
+func (response *InboundCallResponse) WriteArg2(arg Output) error {
+	if err := response.writeArg1(BytesOutput(nil)); err != nil {
 		return err
 	}
 
-	return call.writeArg(arg, false, inboundCallResponseReadyToWriteArg2,
-		inboundCallResponseReadyToWriteArg3)
+	return response.writeArg2(arg)
 }
 
 // WriteArg3 writes the third argument in the response, blocking until the argument is
-// fully written or an error/timeout has occurred.
-func (call *InboundCallResponse) WriteArg3(arg Output) error {
-	return call.writeArg(arg, true, inboundCallResponseReadyToWriteArg3, inboundCallResponseComplete)
-}
-
-func (call *InboundCallResponse) writeArg(arg Output, last bool,
-	inState inboundCallResponseState, outState inboundCallResponseState) error {
-	if call.state != inState {
-		return call.failed(errCallStateMismatch)
-	}
-
-	if err := call.body.WriteArgument(arg, last); err != nil {
-		return call.failed(err)
-	}
-
-	call.state = outState
-	return nil
-}
-
-// Marks the call as failed
-func (call *InboundCallResponse) failed(err error) error {
-	call.state = inboundCallResponseError
-	call.conn.inboundCallComplete(call.id)
-	return err
-}
-
-// newFragment allocates a new fragment to use for writing
-func (call *InboundCallResponse) newFragment(initial bool, checksum Checksum) (*writableFragment, error) {
-	frame := call.conn.framePool.Get()
-	frame.Header.ID = call.id
-	if initial {
-		callRes := callRes{
-			id:           call.id,
-			ResponseCode: responseOK,
-			Headers:      callHeaders{},
-		}
-
-		if call.applicationError {
-			callRes.ResponseCode = responseApplicationError
-		}
-
-		if span := CurrentSpan(call.mex.ctx); span != nil {
-			callRes.Tracing = *span
-		}
-		return newWritableFragment(frame, &callRes, checksum)
-	} else {
-		return newWritableFragment(frame, &callResContinue{}, checksum)
-	}
-}
-
-// flushFragment sends a response fragment back to the peer
-func (call *InboundCallResponse) flushFragment(fragment *writableFragment) error {
-	// TODO(mmihic): This is identical to flushFragment on OutboundCall so unify
-	frame := fragment.frame.(*Frame)
-	frame.Header.SetPayloadSize(uint16(fragment.contents.BytesWritten()))
-	select {
-	case <-call.mex.ctx.Done():
-		return call.failed(call.mex.ctx.Err())
-	case call.conn.sendCh <- frame:
-		return nil
-	default:
-		return call.failed(ErrSendBufferFull)
-	}
+// fully written or an error/timeout has occurred
+func (response *InboundCallResponse) WriteArg3(arg Output) error {
+	return response.writeArg3(arg)
 }
 
 // Manages handlers
