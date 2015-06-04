b36f449a99864285521979121b59643e9a121b45
Raynos <raynos2@gmail.com>
2015-06-04 14:18:19 -0700

    channel: expose AsThrift and AsJSON

--- test/as-json.js
+++ test/as-json.js
@@ -24,7 +24,6 @@
 
 var TypedError = require('error/typed');
 
-var TChannelJSON = require('../as/json.js');
 var allocCluster = require('./lib/alloc-cluster.js');
 
 allocCluster.test('getting an ok response', {
@@ -220,7 +219,7 @@ function makeTChannelJSONServer(cluster, opts) {
         opts.networkFailureResponse ? networkFailureHandler :
             networkFailureHandler;
 
-    var tchannelJSON = TChannelJSON({
+    var tchannelJSON = cluster.channels[0].TChannelAsJSON({
         logParseFailures: false
     });
     tchannelJSON.register(server, 'echo', options, fn);
