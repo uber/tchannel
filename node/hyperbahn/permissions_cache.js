409822c2a7439fddfc1d50964d935a5cab0198c4
Michael Browning <mrbrowning@distopia.net>
2015-06-09 20:54:29 +0000

    replace naive forever-incrementing counts with token buckets

--- node/hyperbahn/permissions_cache.js
+++ node/hyperbahn/permissions_cache.js
@@ -23,6 +23,11 @@
 var inherits = require('util').inherits;
 var LRUCache = require('lru-cache');
 
+var BUCKET_RESET_DURATION = 1000;
+// This is a dummy value until we nail down how, exactly, we'll be getting
+// the actual rate limit from vorenus-controller.
+var NUM_TOKENS = 100;
+
 function PermissionsCache(options) {
     if (!(this instanceof PermissionsCache)) {
         return new PermissionsCache(options);
@@ -34,18 +39,46 @@ function PermissionsCache(options) {
 
     self.channel = self.options.channel;
     self.logger = self.options.logger;
-    self.value = 0;
+    self._bucketIntervals = [];
 
     self.channel.statEvent.addListener(self.increment.bind(self));
 }
 
 inherits(PermissionsCache, LRUCache);
 
+PermissionsCache.prototype.clearBuckets = function clearBuckets() {
+    var self = this;
+    for (var i = 0; i < self._bucketIntervals.length; i++) {
+        clearInterval(self._bucketIntervals[i]);
+    }
+    self.reset();
+};
+
 PermissionsCache.prototype.increment = function increment(stat) {
     var self = this;
     if (stat.name === 'inbound.calls.recvd' && stat.type === 'counter') {
-        var key = createCallsKey(stat.tags['calling-service'], stat.tags.service);
-        self.set(key, (self.get(key) || 0) + 1);
+        var key = createCallsKey(
+            stat.tags['calling-service'], stat.tags.service
+        );
+        var tokens = self.get(key);
+        if (typeof tokens === 'undefined') {
+            self.initializeTokenBucket(key);
+            tokens = self.get(key);
+        }
+
+        self.set(key, tokens - 1);
+    }
+};
+
+PermissionsCache.prototype.initializeTokenBucket =
+function initializeTokenBucket(key) {
+    var self = this;
+    
+    resetTokens();
+    self._bucketIntervals.push(setInterval(resetTokens, BUCKET_RESET_DURATION));
+
+    function resetTokens() {
+        self.set(key, NUM_TOKENS);
     }
 };
 
