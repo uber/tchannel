8d6f9038ec09b59248984b1569e27c52185373cf
Prashant Varanasi <prashant@uber.com>
2015-06-10 19:20:43 -0700

    Fix failing TestOneWay by waiting for server code to be run.
    Test was failing as test asserts expectations, but OneWay does
    not wait for the server to receive the call (semantics of oneway).
    Change code to wait for server calls to be Run before asserting
    expectations.

--- golang/testutils/wait.go
+++ golang/testutils/wait.go
@@ -1,6 +1,7 @@
 package testutils
 
 import (
+	"sync"
 	"time"
 )
 
@@ -24,5 +25,22 @@ func WaitFor(timeout time.Duration, f func() bool) bool {
 		if sleepFor < maxSleep {
 			sleepFor *= 2
 		}
+	}
+}
+
+// WaitWG waits for the given WaitGroup to be complete with a timeout
+// and returns whether the WaitGroup completed within the timeout.
+func WaitWG(wg *sync.WaitGroup, timeout time.Duration) bool {
+	wgC := make(chan struct{})
+
+	go func() {
+		wg.Wait()
+		wgC <- struct{}{}
+	}()
+	select {
+	case <-time.After(timeout):
+		return false
+	case <-wgC:
+		return true
 	}
 }
