8d9160ec4f539f4c17a30bf1f67596ad47cab400
Prashant Varanasi <prashant@uber.com>
2015-06-16 15:20:52 -0700

    go: Use a Configuration type for Hyperbahn so config can be loaded from yaml.

---
+++ golang/hyperbahn/configuration.go
@@ -0,0 +1,7 @@
+package hyperbahn
+
+// Configuration is the initial configuration
+type Configuration struct {
+	// InitialNodes is the list of known Hyperbahn nodes to add initially.
+	InitialNodes []string
+}
