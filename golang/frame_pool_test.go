c7022e6b54641358a692df57b8f72d9674c4aa29
Prashant Varanasi <prashant@uber.com>
2015-06-22 19:15:26 -0700

    Write test to verify that frames are released.

---
+++ golang/frame_pool_test.go
@@ -0,0 +1,186 @@
+package tchannel
+
+// Copyright (c) 2015 Uber Technologies, Inc.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"runtime"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/uber/tchannel/golang/testutils"
+	"golang.org/x/net/context"
+)
+
+type swapper struct {
+	t *testing.T
+}
+
+func (s *swapper) OnError(ctx context.Context, err error) {
+	s.t.Errorf("OnError: %v", err)
+}
+
+func (*swapper) Handle(ctx context.Context, args *rawArgs) (*rawRes, error) {
+	return &rawRes{
+		Arg2: args.Arg3,
+		Arg3: args.Arg2,
+	}, nil
+}
+
+type recordingFramePool struct {
+	mut         sync.Mutex
+	allocations map[*Frame]string
+	badRelease  []string
+}
+
+func newRecordingFramePool() *recordingFramePool {
+	return &recordingFramePool{
+		allocations: make(map[*Frame]string),
+	}
+}
+
+func recordStack() string {
+	buf := make([]byte, 4096)
+	runtime.Stack(buf, false)
+	return string(buf)
+}
+
+func (p *recordingFramePool) Get() *Frame {
+	p.mut.Lock()
+	defer p.mut.Unlock()
+	frame := NewFrame(MaxFramePayloadSize)
+	p.allocations[frame] = recordStack()
+	return frame
+}
+
+func (p *recordingFramePool) Release(f *Frame) {
+	p.mut.Lock()
+	defer p.mut.Unlock()
+
+	if _, ok := p.allocations[f]; !ok {
+		p.badRelease = append(p.badRelease, "bad Release at "+recordStack())
+		return
+	}
+
+	delete(p.allocations, f)
+}
+
+func (p *recordingFramePool) CheckEmpty() (int, string) {
+	p.mut.Lock()
+	defer p.mut.Unlock()
+
+	var badCalls []string
+	badCalls = append(badCalls, p.badRelease...)
+	for f, s := range p.allocations {
+		badCalls = append(badCalls, fmt.Sprintf("frame %v not released, get from: %v", f.Header, s))
+	}
+	return len(p.allocations), strings.Join(badCalls, "\n")
+}
+
+func TestFramesReleased(t *testing.T) {
+	testutils.SetTimeout(t, time.Second*100)
+
+	pool := newRecordingFramePool()
+	origPool := DefaultFramePool
+	DefaultFramePool = pool
+	defer func() { DefaultFramePool = origPool }()
+
+	const (
+		// requestsPerGoroutine = 10
+		// numGoroutines        = 10
+		// maxRandArg           = 512 * 1024
+		requestsPerGoroutine = 1
+		numGoroutines        = 1
+		maxRandArg           = 500 * 1024
+	)
+
+	// Generate random bytes used to create arguments.
+	randBytes := make([]byte, maxRandArg)
+	for i := 0; i < len(randBytes); i += 8 {
+		n := rand.Int63()
+		for j := 0; j < 8; j++ {
+			randBytes[i+j] = byte(n & 0xff)
+			n = n << 1
+		}
+	}
+
+	require.NoError(t, withServerChannel(&testChannelOpts{
+		ServiceName: "swap-server",
+		DefaultConnectionOptions: ConnectionOptions{
+			FramePool: pool,
+		},
+	}, func(serverCh *Channel, hostPort string) {
+		serverCh.Register(AsRaw(&swapper{t}), "swap")
+
+		clientCh, err := NewChannel("swap-client", nil)
+		require.NoError(t, err)
+		defer clientCh.Close()
+
+		generateArg := func(n int) []byte {
+			from := rand.Intn(maxRandArg - n)
+			return randBytes[from : from+n]
+		}
+
+		var wg sync.WaitGroup
+		worker := func() {
+			for i := 0; i < requestsPerGoroutine; i++ {
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+				defer cancel()
+
+				require.NoError(t, clientCh.Ping(ctx, hostPort))
+
+				argSize := rand.Intn(maxRandArg)
+				arg2 := generateArg(argSize)
+				arg3 := generateArg(argSize)
+				resArg2, resArg3, _, err := sendRecv(ctx, clientCh, hostPort, "swap-server", "swap", arg2, arg3)
+				if !assert.NoError(t, err, "error during sendRecv") {
+					continue
+				}
+
+				// We expect the arguments to be swapped.
+				if bytes.Compare(arg3, resArg2) != 0 {
+					t.Errorf("returned arg2 does not match expected:\n  got %v\n want %v", resArg2, arg3)
+				}
+				if bytes.Compare(arg2, resArg3) != 0 {
+					t.Errorf("returned arg2 does not match expected:\n  got %v\n want %v", resArg3, arg2)
+				}
+			}
+			wg.Done()
+		}
+
+		for i := 0; i < numGoroutines; i++ {
+			wg.Add(1)
+			go worker()
+		}
+
+		wg.Wait()
+	}))
+
+	if unreleasedCount, isEmpty := pool.CheckEmpty(); isEmpty != "" || unreleasedCount > 0 {
+		t.Errorf("Frame pool has %v unreleased frames, errors:\n%v", unreleasedCount, isEmpty)
+	}
+}
