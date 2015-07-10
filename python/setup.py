751daeb5e6b628374c0800e51b199262d43f8961
Abhinav Gupta <abg@uber.com>
2015-07-10 15:07:56 -0700

    [python] Back to dev

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tchannel',
-    version='0.10.0',
+    version='0.10.1.dev0',
     author='Abhinav Gupta, Aiden Scandella, Bryce Lampe, Grayson Koonce, Junchao Wu',
     author_email='dev@uber.com',
     description='Network multiplexing and framing protocol for RPC',
