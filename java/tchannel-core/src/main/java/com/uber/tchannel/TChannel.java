bc19ad4a44570286c05ccf43717ac126f1ef0562
Will Salisbury <wjs@uber.com>
2015-07-10 18:54:05 -0700

    Initial commit...again

---
+++ java/tchannel-core/src/main/java/com/uber/tchannel/TChannel.java
@@ -0,0 +1,29 @@
+package com.uber.tchannel;
+
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.ReferenceCountUtil;
+
+public class TChannel extends ChannelHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) { // (1)
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg); // (2)
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
