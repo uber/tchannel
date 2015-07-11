2bb540b91745cbe2d4bbc8d781603623e4a3c19b
Will Salisbury <wjs@uber.com>
2015-07-11 13:33:35 -0700

    Add basic codec test

--- java/tchannel-core/src/main/java/com/uber/tchannel/FrameDecoder.java
+++ java/tchannel-core/src/main/java/com/uber/tchannel/FrameDecoder.java
@@ -7,9 +7,10 @@ import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 public class FrameDecoder extends LengthFieldBasedFrameDecoder {
 
     public static final int MAX_FRAME_LENGTH = 65536;
+    public static final int FRAME_HEADER_LENGTH = 16;
 
     public FrameDecoder() {
-        super(MAX_FRAME_LENGTH,0, 2,-2, 0, true);
+        super(MAX_FRAME_LENGTH, 0, 2, -2, 0, true);
     }
 
     @Override
@@ -21,7 +22,7 @@ public class FrameDecoder extends LengthFieldBasedFrameDecoder {
         in.skipBytes(1);
         long id = in.readUnsignedInt();
         in.skipBytes(8);
-        byte[] payload = new byte[size-16];
+        byte[] payload = new byte[size - FRAME_HEADER_LENGTH];
         in.readBytes(payload);
 
         return new Frame(size, type, id, payload);
