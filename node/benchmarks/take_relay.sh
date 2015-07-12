d5852be88ca918bdfe8f9d22cef66b21f242ae5a
Raynos <raynos2@gmail.com>
2015-07-11 21:26:57 -0700

    benchmarks: make take_relay do multiProc

--- node/benchmarks/take_relay.sh
+++ node/benchmarks/take_relay.sh
@@ -6,5 +6,5 @@ rev=$(git rev-parse --short HEAD)
 sym=$(git symbolic-ref HEAD)
 sym=${sym##*/}
 
-node benchmarks/index.js --relay -o benchmarks/relay-$rev.json -- --relay -m 5 -c 10 -p 1000,10000,20000 -s 4,$(( 2 ** 12 )),$(( 2 ** 14 ))
+node benchmarks/index.js --multiProc --relay -o benchmarks/relay-$rev.json -- --relay -m 5 -c 10 -p 1000,10000,20000 -s 4,$(( 2 ** 12 )),$(( 2 ** 14 ))
 ln -sf relay-$rev.json benchmarks/relay-$sym.json
