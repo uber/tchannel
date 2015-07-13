3cdb55ce02f9ddf2068ee0a900d430d670b8100d
Joshua T Corbin <joshua@uber.com>
2015-07-13 11:20:58 -0700

    Add TimeHeap

--- test/index.js
+++ test/index.js
@@ -66,5 +66,6 @@ require('./busy.js');
 require('./ephemeral-client.js');
 require('./relay-to-dead.js');
 require('./rate-limiter.js');
+require('./time_heap.js');
 
 require('./hyperbahn/')(HyperbahnCluster);
