cff49ad84e0e12b51092de609d61e5c093b8d002
Joshua T Corbin <joshua@uber.com>
2015-07-23 08:27:20 -0700

    Circuits: trivial refactor in #handleRequest

--- node/circuits.js
+++ node/circuits.js
@@ -162,7 +162,8 @@ Circuits.prototype.handleRequest = function handleRequest(req, buildRes, nextHan
         return buildRes().sendError('BadRequest', 'All requests must have a service name');
     }
 
-    var circuit = self.getCircuit(callerName, serviceName, String(req.arg1));
+    var arg1 = String(req.arg1);
+    var circuit = self.getCircuit(callerName, serviceName, arg1);
     return circuit.handleRequest(req, buildRes, nextHandler);
 };
 
