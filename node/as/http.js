1285bfedf60618e2359a8d39896b46e4ba43f550
Anson Qian <abq@uber.com>
2015-07-25 15:33:36 -0700

    Wrap around bufrw error

--- node/as/http.js
+++ node/as/http.js
@@ -83,7 +83,10 @@ TChannelHTTP.prototype.sendRequest = function send(treq, hreq, options, callback
     var arg1 = ''; // TODO: left empty for now, could compute circuit names heuristically
     var arg2res = bufrw.toBufferResult(HTTPReqArg2.RW, head);
     if (arg2res.err) {
-        callback(arg2res.err, null, null);
+        var toBufferErr = errors.HTTPReqArg2toBufferError(arg2res.err, {
+            head: head
+        });
+        callback(toBufferErr, null, null);
         return;
     }
     var arg2 = arg2res.value;
@@ -114,7 +117,10 @@ TChannelHTTP.prototype.sendRequest = function send(treq, hreq, options, callback
         // to ensure singularity
         var arg2res = bufrw.fromBufferResult(HTTPResArg2.RW, arg2);
         if (arg2res.err) {
-            callback(arg2res.err, null, null);
+            var fromBufferErr = errors.HTTPReqArg2fromoBufferError(arg2res.err, {
+                arg2: arg2
+            });
+            callback(fromBufferErr, null, null);
         } else if (tres.streamed) {
             callback(null, arg2res.value, tres.arg3);
         } else {
@@ -137,8 +143,10 @@ TChannelHTTP.prototype.sendResponse = function send(buildResponse, hres, callbac
 
     var arg2res = bufrw.toBufferResult(HTTPResArg2.RW, head);
     if (arg2res.err) {
-        // TODO: wrapped error
-        callback(arg2res.err, null, null);
+        var toBufferErr = errors.HTTPResArg2toBufferError(arg2res.err, {
+            head: head
+        });
+        callback(toBufferErr, null, null);
         buildResponse().sendError('UnexpectedError',
             'Couldn\'t encode as-http response arg2: ' +
             arg2res.err.message);
@@ -295,7 +303,10 @@ AsHTTPHandler.prototype.handleRequest = function handleRequest(req, buildRespons
 
         var arg2res = bufrw.fromBufferResult(HTTPReqArg2.RW, arg2);
         if (arg2res.err) {
-            sendError(arg2res.err);
+            var fromBufferErr = errors.HTTPResArg2fromoBufferError(arg2res.err, {
+                arg2: arg2
+            });
+            sendError(fromBufferErr);
             return;
         }
 
@@ -335,9 +346,8 @@ AsHTTPHandler.prototype.handleRequest = function handleRequest(req, buildRespons
 
     function sendError(err) {
         if (!sent) {
-            // TODO: map type?
             sent = true;
-            buildResponse().sendError('UnexpectedError', err.message);
+            buildResponse().sendError('Declined', err.message);
         }
     }
 };
