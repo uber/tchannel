d2f27cc58b6502ff8ea8426895deb4d829a6ffe2
Raynos <raynos2@gmail.com>
2015-07-27 15:16:03 -0700

    test: inline trace/index.js

--- node/test/trace/lib/server_2_requests_fixture.js
+++ node/test/trace/lib/server_2_requests_fixture.js
@@ -18,7 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-var validators = require('../lib/simple_validators');
+var validators = require('../../lib/simple_validators');
 
 var idStore = {};
 
