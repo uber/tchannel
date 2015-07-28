35fb0e5c6b6469c43da3cc8a9a2895bb7403d1ac
Raynos <raynos2@gmail.com>
2015-07-28 14:02:33 -0700

    tests: rewrite pool of servers test

---
+++ test/pool-of-servers.js
@@ -0,0 +1,102 @@
+// Copyright (c) 2015 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+'use strict';
+
+var parallel = require('run-parallel');
+
+var allocCluster = require('./lib/alloc-cluster.js');
+
+allocCluster.test('request().send() to a pool of servers', {
+    numPeers: 5
+}, function t(cluster, assert) {
+    var client = cluster.channels[0];
+
+    var hosts = cluster.channels.slice(1).map(function hp(c) {
+        return c.hostPort;
+    });
+
+    var clientChannel = client.makeSubChannel({
+        serviceName: 'server',
+        peers: hosts
+    });
+
+    cluster.channels.slice(1).forEach(function each(chan, i) {
+        makeServer(chan, i);
+    });
+
+    var callReqThunks = [];
+    for (var i = 0; i < 200; i++) {
+        var req = clientChannel.request({
+            serviceName: 'server',
+            hasNoParent: true,
+            timeout: 500,
+            headers: {
+                cn: 'client',
+                as: 'raw'
+            }
+        });
+
+        callReqThunks.push(req.send.bind(req, 'foo', 'a', 'b'));
+    }
+
+    parallel(callReqThunks, onResults);
+
+    function onResults(err, results) {
+        assert.ifError(err);
+
+        var byServer = {};
+        for (var j = 0; j < results.length; j++) {
+            var res = results[j];
+            var body = String(res.arg3);
+
+            if (!byServer[body]) {
+                byServer[body] = 0;
+            }
+
+            byServer[body]++;
+        }
+
+        var keys = Object.keys(byServer);
+        assert.equal(keys.length, 4);
+
+        for (var k = 0; k < keys.length; k++) {
+            var count = byServer[keys[k]];
+            console.log('count', count);
+
+            assert.ok(count >= 30, 'count for ' + keys[k] + ' is >= 30');
+            assert.ok(count <= 70, 'count for ' + keys[k] + ' is <= 70');
+        }
+        assert.end();
+    }
+});
+
+function makeServer(channel, index) {
+    var chanNum = index + 1;
+
+    var serverChan = channel.makeSubChannel({
+        serviceName: 'server'
+    });
+
+    serverChan.register('foo', function foo(req, res, arg2, arg3) {
+        res.headers.as = 'raw';
+        res.sendOk(arg2, arg3 + ' served by ' + chanNum);
+    });
+}
