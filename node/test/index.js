eb57e8570b35aa9ed423da8474f6c439a1238f9d
Raynos <raynos2@gmail.com>
2015-07-28 18:35:35 -0700

    tests: refactor test/index to not have side effects

--- node/test/index.js
+++ node/test/index.js
@@ -20,78 +20,78 @@
 
 'use strict';
 
-require('./errors.js');
-require('./event_emitter.js');
-require('./argstream.js');
-require('./circuits.js');
-require('./safe-quit.js');
-require('./timeouts.js');
-require('./send.js');
-require('./retry.js');
-require('./relay.js');
-require('./streaming.js');
-require('./streaming_bisect.js');
-require('./register.js');
-require('./identify.js');
-require('./max_pending.js');
-require('./tchannel.js');
-require('./regression-inOps-leak.js');
-require('./regression-listening-on-used-port.js');
-require('./as-thrift.js');
-require('./as-json.js');
-require('./as-http.js');
-require('./peer.js');
-require('./peers.js');
-require('./streaming-resp-err.js');
-require('./double-response.js');
-require('./ping.js');
-require('./request-stats.js');
-require('./request-with-statsd.js');
-require('./response-stats.js');
-require('./response-with-statsd.js');
-require('./ping.js');
-require('./permissions_cache.js');
-require('./connection-stats.js');
-require('./connection-with-statsd.js');
-require('./request-error-context.js');
-require('./max-call-overhead.js');
-require('./non-zero-ttl.js');
-require('./examples.js');
-require('./busy.js');
-require('./ephemeral-client.js');
-require('./relay-to-dead.js');
-require('./rate-limiter.js');
-require('./time_heap.js');
-require('./balance_peer_requests.js');
-require('./tcollector-reporter.js');
-require('./pool-of-servers.js');
-
-require('./trace/basic_server.js');
-require('./trace/server_2_requests.js');
-require('./trace/outpeer_span_handle.js');
-
-require('./v2/frame.js');
-require('./v2/init.js');
-require('./v2/checksum.js');
-require('./v2/header.js');
-require('./v2/tracing.js');
-require('./v2/call.js');
-require('./v2/cancel.js');
-require('./v2/cont.js');
-require('./v2/claim.js');
-require('./v2/ping.js');
-require('./v2/error_response.js');
-require('./v2/args.js');
-require('./v2/lazy_frame.js');
-
-require('./hyperbahn/constructor.js');
-require('./hyperbahn/todo.js');
-require('./hyperbahn/sub-channel.js');
-require('./hyperbahn/kill-switch.js');
-
 module.exports = runTests;
 
 if (require.main === module) {
+    require('./errors.js');
+    require('./event_emitter.js');
+    require('./argstream.js');
+    require('./circuits.js');
+    require('./safe-quit.js');
+    require('./timeouts.js');
+    require('./send.js');
+    require('./retry.js');
+    require('./relay.js');
+    require('./streaming.js');
+    require('./streaming_bisect.js');
+    require('./register.js');
+    require('./identify.js');
+    require('./max_pending.js');
+    require('./tchannel.js');
+    require('./regression-inOps-leak.js');
+    require('./regression-listening-on-used-port.js');
+    require('./as-thrift.js');
+    require('./as-json.js');
+    require('./as-http.js');
+    require('./peer.js');
+    require('./peers.js');
+    require('./streaming-resp-err.js');
+    require('./double-response.js');
+    require('./ping.js');
+    require('./request-stats.js');
+    require('./request-with-statsd.js');
+    require('./response-stats.js');
+    require('./response-with-statsd.js');
+    require('./ping.js');
+    require('./permissions_cache.js');
+    require('./connection-stats.js');
+    require('./connection-with-statsd.js');
+    require('./request-error-context.js');
+    require('./max-call-overhead.js');
+    require('./non-zero-ttl.js');
+    require('./examples.js');
+    require('./busy.js');
+    require('./ephemeral-client.js');
+    require('./relay-to-dead.js');
+    require('./rate-limiter.js');
+    require('./time_heap.js');
+    require('./balance_peer_requests.js');
+    require('./tcollector-reporter.js');
+    require('./pool-of-servers.js');
+
+    require('./trace/basic_server.js');
+    require('./trace/server_2_requests.js');
+    require('./trace/outpeer_span_handle.js');
+
+    require('./v2/frame.js');
+    require('./v2/init.js');
+    require('./v2/checksum.js');
+    require('./v2/header.js');
+    require('./v2/tracing.js');
+    require('./v2/call.js');
+    require('./v2/cancel.js');
+    require('./v2/cont.js');
+    require('./v2/claim.js');
+    require('./v2/ping.js');
+    require('./v2/error_response.js');
+    require('./v2/args.js');
+    require('./v2/lazy_frame.js');
+
+    require('./hyperbahn/constructor.js');
+    require('./hyperbahn/todo.js');
+    require('./hyperbahn/sub-channel.js');
+    require('./hyperbahn/kill-switch.js');
+
     runTests(require('./lib/hyperbahn-cluster.js'));
 }
 
