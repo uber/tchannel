49cad1946edbcfef85df3a72a60efbe71e34aeea
Juncao Li <juncaoli@uber.com>
2015-08-03 17:02:38 -0700

    rate limiting log: more fields

--- hyperbahn/service_proxy.js
+++ hyperbahn/service_proxy.js
@@ -141,8 +141,11 @@ function rateLimit(req, buildRes) {
     if (self.rateLimiter.shouldRateLimitTotalRequest(req.serviceName)) {
         var totalLimit = self.rateLimiter.totalRequestCounter.rpsLimit;
         self.logger.info('hyperbahn node is rate-limited by the total rps limit', {
+            rpsLimit: totalLimit,
             cn: req.headers && req.headers.cn,
-            rpsLimit: totalLimit
+            serviceName: req.serviceName,
+            endpoint: req.endpoint,
+            remoteAddr: req.remoteAddr
         });
         buildRes().sendError('Busy', 'hyperbahn node is rate-limited by the total rps of ' + totalLimit);
         return true;
@@ -152,9 +155,11 @@ function rateLimit(req, buildRes) {
     if (isExitNode && self.rateLimiter.shouldRateLimitService(req.serviceName)) {
         var serviceLimit = self.rateLimiter.getRpsLimitForService(req.serviceName);
         self.logger.info('hyperbahn service is rate-limited by the service rps limit', {
+            rpsLimit: serviceLimit,
             cn: req.headers && req.headers.cn,
             serviceName: req.serviceName,
-            rpsLimit: serviceLimit
+            endpoint: req.endpoint,
+            remoteAddr: req.remoteAddr
         });
         buildRes().sendError('Busy', req.serviceName + ' is rate-limited by the rps of ' + serviceLimit);
         return true;
