96278c454f423b17ff42c70900ad887711065cfc
Prashant Varanasi <prashant@uber.com>
2015-08-05 18:55:39 -0700

    Add test to verify NewClient parses InitialNodesList

---
+++ golang/hyperbahn/client_test.go
@@ -0,0 +1,111 @@
+package hyperbahn
+
+import (
+	"io/ioutil"
+	"os"
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/uber/tchannel/golang"
+	"github.com/uber/tchannel/golang/testutils"
+)
+
+// Copyright (c) 2015 Uber Technologies, Inc.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+func getPeers(ch *tchannel.Channel) []string {
+	var peers []string
+	for k := range ch.Peers().Copy() {
+		peers = append(peers, k)
+	}
+	sort.Strings(peers)
+	return peers
+}
+
+func TestParseConfiguration(t *testing.T) {
+	expectedPeers1 := []string{"1.1.1.1:1", "2.2.2.2:2"}
+	expectedPeers2 := []string{"3.3.3.3:3", "4.4.4.4:4"}
+	peersFile2 := `["3.3.3.3:3", "4.4.4.4:4"]`
+
+	tests := []struct {
+		name      string
+		peersArg  []string
+		peersFile string
+		wantPeers []string
+		wantErr   bool
+	}{
+		{
+			name:    "no peers",
+			wantErr: true,
+		},
+		{
+			name:      "no peer list",
+			peersArg:  expectedPeers1,
+			wantPeers: expectedPeers1,
+		},
+		{
+			name:      "peer file",
+			peersFile: peersFile2,
+			wantPeers: expectedPeers2,
+		},
+		{
+			name:      "peer file overrides args",
+			peersArg:  expectedPeers1,
+			peersFile: peersFile2,
+			wantPeers: expectedPeers2,
+		},
+	}
+
+	for _, tt := range tests {
+		peerFile := ""
+		if tt.peersFile != "" {
+			f, err := ioutil.TempFile("", "hosts")
+			if !assert.NoError(t, err, "%v: TempFile failed", tt.name) {
+				continue
+			}
+			defer os.Remove(f.Name())
+
+			_, err = f.WriteString(tt.peersFile)
+			assert.NoError(t, err, "%v: write peer file failed", tt.name)
+
+			assert.NoError(t, err, "%v: write peer file failed", tt.name)
+			assert.NoError(t, f.Close(), "%v: close peer file failed", tt.name)
+			peerFile = f.Name()
+		}
+
+		config := Configuration{InitialNodes: tt.peersArg, InitialNodesFile: peerFile}
+		ch, err := testutils.NewClient(nil)
+		require.NoError(t, err, "%v: testutils.NewClient failed", tt.name)
+		defer ch.Close()
+
+		_, err = NewClient(ch, config, nil)
+		if tt.wantErr {
+			assert.Error(t, err, "%v: NewClient expected to fail")
+			continue
+		}
+		if !assert.NoError(t, err, "%v: hyperbahn.NewClient failed", tt.name) {
+			continue
+		}
+
+		assert.Equal(t, tt.wantPeers, getPeers(ch), "%v: got unexpected peers", tt.name)
+	}
+}
