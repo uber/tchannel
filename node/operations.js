ed4e323a2f2d3be54a35802cbacdb33abb232fac
Raynos <raynos2@gmail.com>
2015-08-11 21:13:03 -0700

    operations: add more meta information to stale tombstone

--- node/operations.js
+++ node/operations.js
@@ -330,6 +330,9 @@ Operations.prototype._sweepOps = function _sweepOps(ops, direction) {
                 self.popOutReq(id);
             }
         } else if (op.isTombstone) {
+            var heap = self.connection.channel.timeHeap;
+            var expireTime = op.time + op.timeout;
+
             if (!op.operations) {
                 self.logger.warn('zombie tombstone', op.extendLogInfo({
                     direction: direction,
@@ -339,10 +342,16 @@ Operations.prototype._sweepOps = function _sweepOps(ops, direction) {
                 op.operations = null;
                 op.timeHeapHandle.cancel();
                 op.timeHeapHandle = null;
-            } else if (op.time + op.timeout < now) {
+            } else if (op.time + op.timeout < now &&
+                heap.lastRun > expireTime
+            ) {
                 self.logger.warn('stale tombstone', op.extendLogInfo({
                     direction: direction,
-                    opKey: id
+                    opKey: id,
+                    now: now,
+                    staleDelta: op.time + op.timeout - now,
+                    expireTime: expireTime,
+                    heapLastRun: self.connection.channel.timeHeap.lastRun
                 }));
                 delete ops[id];
                 op.operations = null;
