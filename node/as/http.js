874ce1c059a42c5f2980c965662bfc66f86314c4
Anson Qian <abq@uber.com>
2015-08-26 10:52:18 -0700

    Fix lbpool encoding

--- node/as/http.js
+++ node/as/http.js
@@ -311,7 +311,9 @@ TChannelHTTP.prototype.forwardToHTTP = function forwardToHTTP(tchannel, options,
 
 TChannelHTTP.prototype._forwardToLBPool = function _forwardToLBPool(options, inreq, outres, callback) {
     var self = this;
-
+    options = extend(options, {
+        encoding: null
+    });
     var data = inreq.bodyStream || inreq.bodyArg; // lb_pool likes polymorphism
     self.lbpool.request(options, data , onResponse);
 
