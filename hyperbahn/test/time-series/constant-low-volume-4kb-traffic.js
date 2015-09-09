b6f8eb89009d182f513a3a48a242b3b3fe3f2108
Raynos <raynos2@gmail.com>
2015-09-09 15:52:08 -0700

    add more events

---
+++ hyperbahn/test/time-series/constant-low-volume-4kb-traffic.js
@@ -0,0 +1,64 @@
+// Copyright (c) 2015 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+'use strict';
+
+var TimeSeriesCluster = require('./_time-series-cluster.js');
+
+TimeSeriesCluster.test('control test for time-series of timeout requests', {
+    clusterOptions: {},
+    buckets: [
+        25, 25, 25, 25, 25, 25
+    ],
+    clientTimeout: 100,
+    numBatchesInBucket: 10,
+    clientBatchDelay: 1000,
+    clientRequestsPerBatch: 150,
+    endpointToRequest: 'slow-endpoint'
+}, function t(cluster, assert) {
+    cluster.logger.whitelist('info', 'error for timed out outgoing response');
+    cluster.logger.whitelist('info', 'forwarding expected error frame');
+    cluster.logger.whitelist('info', 'expected error while forwarding');
+    cluster.logger.whitelist('info', 'OutResponse.send() after inreq timed out');
+    cluster.logger.whitelist('warn', 'forwarding error frame');
+
+    cluster.batchClient.on('batch-updated', function onUpdate(meta) {
+        console.log('batch updated', {
+            batch: meta.batch
+        });
+    });
+
+    cluster.sendRequests(onResults);
+
+    function onResults(err, results) {
+        assert.ifError(err);
+
+        for (var i = 0; i < cluster.buckets.length; i++) {
+            cluster.assertRange(assert, {
+                value: results[i].errorCount,
+                min: MIN_ERRORS[i],
+                max: MAX_ERRORS[i],
+                name: cluster.buckets[i]
+            });
+        }
+
+        assert.end();
+    }
+});
