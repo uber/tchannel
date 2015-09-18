b2a7e510e11bd5191d3bb9d9df52097dad5b453f
Raynos <raynos2@gmail.com>
2015-09-17 19:38:37 -0700

    improve busy hyperbahn worker tests

--- hyperbahn/test/time-series/making-requests-with-a-single-busy-hyperbahn-worker.js
+++ hyperbahn/test/time-series/making-requests-with-a-single-busy-hyperbahn-worker.js
@@ -20,8 +20,35 @@
 
 'use strict';
 
+var console = require('console');
+
 var TimeSeriesCluster = require('../lib/time-series-cluster.js');
 
+// 14 is a magic number for zero errors + >25 rate limits.
+var OVERLOADED_WORKER_RATELIMIT = 14;
+
+/* End to end test to ensure that retries on Busy work as expected.
+
+    The semantics we want is that when a single Hyperbahn worker
+    is busy an edge client will use retries to "maneouver" around
+    that worker and get 100% success rate.
+
+    We create a TimeSeriesCluster with:
+
+     - 10 hyperbahn worker nodes
+     - rate limiting set to 200 per worker.
+     - We split the test run into 4 buckets
+     - We want one client to have deterministic peer selection
+     - We want 250 req/s split between 10 workers
+     - We manually set the first bucket to 100 req/s to warm
+        up the entire cluster
+
+    Once our test finishes we expect:
+
+     - All requests to succeed
+     - To get at least 2% of requests to be rate limited
+
+*/
 TimeSeriesCluster.test('testing worker with low rate limit', {
     clusterOptions: {
         size: 10,
@@ -36,7 +63,7 @@ TimeSeriesCluster.test('testing worker with low rate limit', {
     buckets: [
         25, 25, 25, 25
     ],
-    clientTimeout: 100,
+    clientTimeout: 500,
     endpointToRequest: 'echo-endpoint',
     requestBody: TimeSeriesCluster.buildString(10),
 
@@ -68,6 +95,8 @@ TimeSeriesCluster.test('testing worker with low rate limit', {
     function onResults(err, results) {
         assert.ifError(err);
 
+        // Ignore first bucket as it's the warmup bucket
+        // which is a statistical outlier for this test.
         for (var i = 1; i < cluster.buckets.length; i++) {
             cluster.assertRange(assert, {
                 value: results[i].errorCount,
@@ -76,21 +105,35 @@ TimeSeriesCluster.test('testing worker with low rate limit', {
                 description: ' error rate ',
                 index: i
             });
+
+            if (results[i].errorCount > 0) {
+                /* eslint no-console: 0 */
+                console.log('# results', results[i]);
+            }
         }
 
         var logLines = cluster.logger.items();
+
+        // We made 250 req/s for 4 seconds. We expect at least
+        // 1 requests out of a 1000 to be rate limited by
+        // worker[4]
         var rateLimits = logLines.filter(isRateLimit);
-        assert.ok(rateLimits.length >= 25,
-            'expect at least 25 attempts to be rate limited');
+        assert.ok(rateLimits.length >= 1,
+            'expect at least 1 attempts to be rate limited ' +
+            'but is: ' + rateLimits.length);
 
         assert.ok(rateLimits.every(function checkLog(r) {
-            return r.fields.rpsLimit === 10;
-        }), 'rate limit should be 10');
+            if (r.meta.rpsLimit !== OVERLOADED_WORKER_RATELIMIT) {
+                console.log('wat', r.meta);
+            }
+
+            return r.meta.rpsLimit === OVERLOADED_WORKER_RATELIMIT;
+        }), 'rate limit should be ' + OVERLOADED_WORKER_RATELIMIT);
 
         assert.end();
     }
 });
 
 function isRateLimit(logRecord) {
-    return logRecord.fields.msg === 'hyperbahn node is rate-limited by the total rps limit';
+    return logRecord.msg === 'hyperbahn node is rate-limited by the total rps limit';
 }
