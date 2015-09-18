e5446898418e032777bb8aa2b05656bfb4df2207
Russ Frank <rf@uber.com>
2015-09-18 00:20:09 +0000

    larch: log number of dropped logs

--- hyperbahn/lib/larch/reservoir-backend.js
+++ hyperbahn/lib/larch/reservoir-backend.js
@@ -26,6 +26,7 @@ var timers = require('timers');
 var NullStatsd = require('uber-statsd-client/null');
 
 var BaseBackend = require('./base-backend');
+var Record = require('./record');
 
 module.exports = ReservoirBackend;
 
@@ -128,6 +129,18 @@ ReservoirBackend.prototype.flush = function flush(records) {
     var self = this;
 
     var start = self.now();
+
+    if (self.count > self.size) {
+        self.backend.log(new Record(
+            'warn',
+            'dropped logs',
+            {
+                dropCount: self.dropCount,
+                flushInterval: self.flushInterval,
+                size: self.size
+            }
+        ));
+    }
 
     var i;
     var keys = Object.keys(self.dropCount);
