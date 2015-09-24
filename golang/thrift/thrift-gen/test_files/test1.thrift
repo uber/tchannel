7fc178e931006538536e384727aaa63c9da83a8e
Prashant Varanasi <prashant@uber.com>
2015-09-24 11:45:54 -0700

    Thrift 1.0.0-dev doesn't allow package as arg name

--- golang/thrift/thrift-gen/test_files/test1.thrift
+++ golang/thrift/thrift-gen/test_files/test1.thrift
@@ -5,7 +5,7 @@ struct FakeStruct {
 }
 
 service Fake {
-  void fAkE(1: string LoL_http_TEST_Name, 2: i32 func, 3: i32 package, 4: i64 user_id, 5: i64 id, 6: FakeStruct fakeStruct)
+  void fAkE(1: string LoL_http_TEST_Name, 2: i32 func, 3: i32 pkg, 4: i64 user_id, 5: i64 id, 6: FakeStruct fakeStruct)
 
   void MyArgs()
   void MyResult()
