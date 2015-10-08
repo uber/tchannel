383909b59d4c969be569826dc6e371b48f2435da
Juncao Li <juncaoli@uber.com>
2015-10-08 11:31:42 -0700

    also use filename for entryPoint

--- thrift/meta.thrift
+++ thrift/meta.thrift
@@ -9,7 +9,7 @@ struct ThriftIDLs {
     // map: filename -> contents
     1: required map<filename, string> idls
     // the entry IDL that imports others
-    2: required string entryPoint
+    2: required filename entryPoint
 }
 
 service Meta {
